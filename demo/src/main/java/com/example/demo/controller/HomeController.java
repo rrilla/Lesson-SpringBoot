d4f28b068095d7e3dc76325073718d108774162b
메돌이 <68496830+rrilla@users.noreply.github.com>
2020-10-07 16:34:56 +0900

    Spring boot 프로젝트 생성,실행

---
+++ demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController	//데이터 리턴,, 뷰 리졸버가 발동안함
+public class HomeController {
+
+	@GetMapping("/home")
+	public String home() {
+		return "Home";
+	}
+}
